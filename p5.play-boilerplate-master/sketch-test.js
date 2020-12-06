f7711b892942708caa8c6fb2453aaafb9a5d8977
Keleecia <72576841+Keleecia@users.noreply.github.com>
2020-12-06 11:04:53 -0800

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch-test.js
@@ -0,0 +1,23 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var ground, engine,world;
+
+function setup() {
+  createCanvas(480,800);
+  createSprite(400, 200, 50, 50);
+  
+  engine = Engine.create();
+  world = engine.world;
+   
+  ground = new Ground(200,200,200,200);
+}
+
+function draw() {
+  background(255,255,255); 
+  Engine.update(engine); 
+  ground.display();
+  drawSprites();
+}
